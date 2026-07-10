cc514bb72fd1b411d043c8cbf6769251e78d4c96
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-10 15:52:43 +0200

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -435,7 +435,7 @@ requestAnimationFrame(() => {
         </div>
 
         {/* Desktop split section */}
-        <<div data-no-reveal className="hidden lg:grid lg:grid-cols-2">
+        <div data-no-reveal className="hidden lg:grid lg:grid-cols-2">
           <div className="flex min-h-[760px] items-start lg:pl-[48px] lg:pr-10 py-20">
             <div className="w-full max-w-none">
               <h2
