6234ff3165b6551216d4ce1e71d38b995af3bd4e
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-13 12:49:39 +0200

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -454,7 +454,7 @@ requestAnimationFrame(() => {
         alt={s.title}
         loading="eager"
         decoding="sync"
-        className="block w-full aspect-[16/7] object-cover"
+        className="block w-full aspect-[4/3] object-cover"
         style={{
           opacity: 1,
           transform: "none",
