bb0c0309a374fe76a755c892b117375a6ce970a6
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-15 11:53:21 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -279,7 +279,7 @@ useLayoutEffect(() => {
           {t("Sustainability", "Nachhaltigkeit")}
         </h2>
 
-        <div className="mt-24">
+        <div className="lg:mt-20 xl:mt-24">
           <SolarImageCard
             mobileText={t(
               "Solar power generated directly on our production site.",
