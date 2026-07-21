d73a35844522a1d62dcb3b0f97817724a9f921c6
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-21 11:09:50 +0200

    Update EditorialFooter.tsx

--- src/components/EditorialFooter.tsx
+++ src/components/EditorialFooter.tsx
@@ -9,7 +9,7 @@ const FooterCertificationLogos = () => (
     <img
       src={grsLogo}
       alt="Global Recycled Standard"
-      className="w-[170px] md:w-[170px] bg-white object-contain"
+      className="w-[250px] md:w-[200px] bg-white object-contain"
     />
   </div>
 );
