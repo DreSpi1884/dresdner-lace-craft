34727bbd03ce68c4bfd365aefcea9b34195d94de
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-24 10:09:24 +0200

    Update ProductionAnchorNav.tsx

--- src/components/ProductionAnchorNav.tsx
+++ src/components/ProductionAnchorNav.tsx
@@ -7,7 +7,7 @@ const getDesktopOffset = () => {
   const subNav = document.querySelector<HTMLElement>("[data-anchor-subnav]");
   const headerHeight = header?.getBoundingClientRect().height ?? 96;
   const subNavHeight = subNav?.getBoundingClientRect().height ?? 0;
-  return headerHeight + subNavHeight + 0;
+  return headerHeight + subNavHeight;
 };
 
 const ProductionAnchorNav = () => {
