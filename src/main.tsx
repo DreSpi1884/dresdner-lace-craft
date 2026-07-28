85ce0903fc0e364d38a43641b5cf76b8437acefe
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-28 12:45:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import { LanguageProvider } from "./i18n/LanguageContext.tsx";
+import "@fontsource-variable/jost/wght.css";
+import "@fontsource-variable/jost/wght-italic.css";
+import "@fontsource-variable/bodoni-moda/wght.css";
+import "@fontsource-variable/bodoni-moda/wght-italic.css";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
