113439866fc2255149707209bee45754aad3e6df
DreSpi1884 <aschroeder@dresdnerspitzen.com>
2026-07-28 11:54:05 +0200

    Update logo image size in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -29,7 +29,7 @@ const FooterCertificationLogos = () => (
       <img
         src={grsLogo}
         alt="Global Recycled Standard"
-        className="w-[200px] md:w-[200px] bg-white object-contain"
+        className="w-[250px] md:w-[300px] bg-white object-contain"
       />
     </a>
     <img
